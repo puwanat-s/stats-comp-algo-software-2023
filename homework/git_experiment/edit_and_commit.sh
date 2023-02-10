16faf9fefcdcc33fe8568970927bba5a3a2e42d9
Puwanat Sangkhapreecha <psangkha@jhpce01.cm.cluster>
2023-02-10 10:54:23 -0500

    Submit edit_and_commit.sh and hw1.Rmd with answers

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -3,5 +3,8 @@
 for commit_count in {2..100}
 do
   let linenum="10+$commit_count"
-  # Fill in
+  sed -i "$linenum i Add a sentence" foo.tex
+  pdflatex foo.tex > /dev/null 2>&1
+  git add foo.pdf foo.tex
+  git commit -m "Added sentence #$commit_count"
 done
