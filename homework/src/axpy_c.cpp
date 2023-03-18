ff6be0d217979683136eaacec5264c047250895d
Puwanat Sangkhapreecha <a.puwanat@gmail.com>
2023-03-18 14:40:12 -0400

    edit axpy_c.cpp and matmat.cpp

--- homework/src/axpy_c.cpp
+++ homework/src/axpy_c.cpp
@@ -1,11 +1,21 @@
 #include <Rcpp.h>
 using Rcpp::NumericVector;
+using Rcpp::stop;
 
 // [[Rcpp::export]]
 NumericVector axpy_c(double a, NumericVector& x, NumericVector& y) {
   int n = x.size();
-  for (int i = 0; i < n; ++i) {
-    y[i] += a * x[i];
+  int m = y.size();
+  
+  if (n != m) {
+    stop("Error: x and y must be of the same length");
   }
-  return y;
+  
+  NumericVector result(n);
+  
+  for (int i = 0; i < n; ++i) {
+    result[i] = y[i] + a * x[i];
+  }
+  
+  return result;
 }
--- homework/src/matmat.cpp
+++ homework/src/matmat.cpp
@@ -14,6 +14,13 @@ NumericMatrix row_col_dot_matmat(
   int n_col_out = B.ncol();
   NumericMatrix result(n_row_out, n_col_out);
   // Fill in; remember to use (,) instead of [,] for accessing matrix elements
+  for (int i = 0; i < n_row_out; ++i) {
+    for (int j = 0; j < n_col_out; ++j) {
+      for (int k = 0; k < A.ncol(); ++k) {
+        result(i,j) += A(i,k) * B(k,j);
+      }
+    }
+  }
   return result;
 }
 
@@ -30,6 +37,10 @@ NumericMatrix col_oriented_matmat(
   for (int j = 0; j < n_col_out; ++j) {
     // Calculate result[, j] = A %*% B[, j] in column-oriented manner for each j
     // Fill in
+    for (int k = 0; k < A.ncol(); ++k) {
+      for (int l = 0; l < n_row_out; ++l)
+        result(l,j) += A(l,k) * B(k,j);
+    }
   }
   return result;
 }
